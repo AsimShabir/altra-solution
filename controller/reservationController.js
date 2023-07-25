b56ee4538ca378ec2759fa645ebe89c227bc2c8c
Asim <asimshabbir4578@gmail.com>
2023-07-25 19:30:25 +0500

    reservation

---
+++ controller/reservationController.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+app.use(cors());
+app.use(express.json());
+const Reservatiin = require("../collections/reservation");
+const Billboard = require("../collections/billboard");
+const User = require("../collections/user");
+
+const BillboardStatus = {
+  Available: "Available",
+  Booked: "Booked",
+};
+const reservation = async (req, res) => {
+  try {
+    const { startDate, endDate, rate, userId, billboardId } = req.body;
+    const user = await User.findById(userId);
+    const billBoard = await Billboard.findById(billboardId);
+
+    if (!user) {
+      return res
+        .status(404)
+        .send({ success: false, message: "User not found." });
+    }
+    if (!billBoard) {
+      return res
+        .status(404)
+        .send({ success: false, message: "Billboard not found." });
+    }
+    const oneDay = 24 * 60 * 60 * 1000; // Number of milliseconds in a day
+    const startDateTime = new Date(startDate).getTime();
+    const endDateTime = new Date(endDate).getTime();
+    const differenceInMilliseconds = endDateTime - startDateTime;
+    const daysBetween = Math.round(differenceInMilliseconds / oneDay) + 1;
+    const totalRate = billBoard.perDayRate * daysBetween;
+
+    if (user && billBoard) {
+      await Reservatiin.create({
+        startDate,
+        endDate,
+        rate: totalRate,
+        billBoard: billBoard.id,
+        user: user.id,
+      });
+      await Billboard.updateOne(
+        { _id: billBoard.id },
+        { $set: { status: BillboardStatus.Booked } }
+      );
+
+      res.send("Bill board reserved");
+    }
+  } catch (e) {
+    res.status(500).send(e);
+  }
+};
+
+module.exports = {
+  reservation,
+};
--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,11 @@ const express = require("express");
 const router = express.Router();
 const user = require("../controller/userController");
 const billboard = require("../controller/billBoardController");
+const reservation = require("../controller/reservationController");
 
 router
   .get("/getallusers", user.getAllUsers)
   .post("/createbillboard", billboard.createBillboard)
-  .get("/getallbillboard", billboard.getAllBillboards);
+  .get("/getallbillboard", billboard.getAllBillboards)
+  .post("/reservation", reservation.reservation);
 module.exports = router;
