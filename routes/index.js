37072d734aecc93c5d0312280a3510e58bd09984
super boy <superboyfarooq@gmail.com>
2023-07-26 07:57:27 +0500

    feat store images on server

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,10 @@ const express = require("express");
 const router = express.Router();
 const user = require("../controller/userController");
 const billboard = require("../controller/billBoardController");
+const midleware = require("../midleware/storeImage");
 
 router
   .get("/getallusers", user.getAllUsers)
-  .post("/createbillboard", billboard.createBillboard)
+  .post("/createbillboard", midleware.storeImage, billboard.createBillboard)
   .get("/getallbillboard", billboard.getAllBillboards);
 module.exports = router;
